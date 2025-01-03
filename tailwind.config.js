54a9b929e3a9f55a21108f9fdc57178d5ea37098
Mastu-Muhammed <muhammedmastu123@gmail.com>
2025-01-03 21:34:42 +0530

    f

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/*.html"],
+ // content: ["./dist/*.html"],
+   content:'./src/**/*.{html,js}',
   theme: {
     extend: {
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+    },
       fill: ['hover'], // Ensures hover states for `fill` are generated
+      fontSize: {
+        sm: ['0.875rem', '1.25rem'], // Small size
+        lg: ['1.125rem', '1.75rem'], // Large size
+      },
+      fontFamily: {
+        primary: ["Roboto", "sans-serif"],
+      },
     },
   },
   plugins: [],
